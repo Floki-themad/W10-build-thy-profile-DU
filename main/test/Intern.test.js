9b2449b3ba159007426daf0b97c874882316003a
Floki <ypr_tylerkeuler@aol.com>
2022-06-22 18:58:01 -0400

    intern test

---
+++ main/test/Intern.test.js
@@ -0,0 +1,9 @@
+const Intern = require(`../lib/Intern`);
+
+describe('Intern', () => {
+    it(`Should return the school that the intern is attending`, () => {
+        const obj1 = new Intern('tyler', 88, `none`, `HardKnox`)
+        
+        expect(obj1.school).toEqual('HardKnox');
+    });
+});
