b623f5af2f10cf66a3115c040a3e7d298409fbc9
Floki <ypr_tylerkeuler@aol.com>
2022-06-22 19:23:58 -0400

    employee constructer class

--- main/test/Intern.test.js
+++ main/test/Intern.test.js
@@ -2,7 +2,7 @@ const Intern = require(`../lib/Intern`);
 
 describe('Intern', () => {
     it(`Should return the school that the intern is attending`, () => {
-        const obj1 = new Intern('tyler', 88, `none`, `HardKnox`)
+        const obj1 = new Intern('tyler', 66, `none`, `HardKnox`)
         
         expect(obj1.school).toEqual('HardKnox');
     });
