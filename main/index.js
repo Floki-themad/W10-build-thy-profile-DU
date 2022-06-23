ab6ae47e0553c134618d61b9006e81e15dfabaf9
Floki <ypr_tylerkeuler@aol.com>
2022-06-22 20:49:42 -0400

    added the inquirer prompt function for each individual

--- main/index.js
+++ main/index.js
@@ -7,20 +7,121 @@ const makeHTMl = require('./src/template');
 const managerStyle = require('./src/InternStyle');
 const engineerStyle = require('./src/Engineerstyle');
 const internStyle = require('./src/InternStyle');
-const {profile} = require('console');
-let teamName ="";
-const teamArray =[];
+const { profile } = require('console');
+const Employee = require('./lib/Employee');
+let teamName = "";
+const teamArray = [];
 
-// start inquirer 
+const start = () => {
+    inquirer.prompt([{
+        type: `input`,
+        name: `teamName`,
+        message: `Whats your team name?`
+    }])
+        .then((data => {
+            teamName = data.teamName;
+            manager();
+        }))
 
-// manager function
+    function manager() {
+        inquirer.prompt([{
+            type: `input`,
+            name: `name`,
+            message: `What is the name of the teams manager?`
+        }, {
+            type: `input`,
+            name: `id`,
+            message: `What is the mangers ID number?`
+        }, {
+            type: `input`,
+            name: `email`,
+            message: `What is the managers email adress?`
+        }, {
+            type: `input`,
+            name: `office`,
+            message: `What is the managers office number?`
+        },
+        ])
+            .then((data) => {
+                let newManager;
+                newManager = new Manager(data.name, data.id, data.email, data.office);
+                teamArray.push(newManager);
+                employee();
+            })
 
-//employee function
+    }
+    function employee() {
+        inquirer.prompt([{
+            type: `list`,
+            name: `continue`,
+            message: ` would you like to add a member to the team?`,
+            choices: [`Engineer`, `Intern`, `No new members`]
+        }])
+            .then((data) => {
+                switch (data.continue) {
+                    case `Engineer`:
+                        engineer();
+                        break;
+                    case `Intern`:
+                        intern();
+                        break;
+                    default:
+                        writePage();
+                        break;
+                }
+            })
+    }
+    function engineer() {
+        inquirer.prompt([{
+            type: `input`,
+            name: `name`,
+            message: `what is the name of the Engineer?`
+        }, {
+            type: `input`,
+            name: `id`,
+            message: `what is the engineers ID?`,
+        }, {
+            type: `input`,
+            name: `email`,
+            message: `what is the engineers email adress?`
+        }, {
+            type: `input`,
+            name: `github`,
+            message: `what is the engineers Github username?`
+        },
+        ])
+            .then((data) => {
+                let newEngineer;
+                newEngineer = new Engineer(data.name, data.id, data.email, data.github);
+                teamArray.push(newEngineer);
+                employee();
+            })
+    }
 
-//engineer function
-
-//intern function 
-
-//write page function 
-
-// init function 
+    function intern() {
+        inquirer.prompt([{
+            type: `input`,
+            name: `name`,
+            message: `what is the name of the intern?`
+        }, {
+            type: `input`,
+            name: `id`,
+            message: `what is the interns ID?`
+        }, {
+            type: `input`,
+            name: `email`,
+            message: `what is the interns email adress?`
+        }, {
+            type: `input`,
+            name: `school`,
+            message: `what school does the intern attend`,
+        },
+        ])
+            .then((data) => {
+                let newIntern;
+                newIntern = new Intern(data.name, data.id, data.email, data.school);
+                teamArray.push(newIntern);
+                employee();
+            })
+    }
+}
