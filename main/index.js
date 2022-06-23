db460aac3d638a22311e31ad1b3a5198c2875dcb
Floki <ypr_tylerkeuler@aol.com>
2022-06-22 20:05:28 -0400

    setting variables for inquirer to require

---
+++ main/index.js
@@ -0,0 +1,12 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const makeHTMl = require('./src/template');
+const managerStyle = require('./src/InternStyle');
+const engineerStyle = require('./src/Engineerstyle');
+const internStyle = require('./src/InternStyle');
+const {profile} = require('console');
+let teamName ="";
+const teamArray =[];
